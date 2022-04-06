2ef3695fdff7b0cff249ccbc8e593537cab15c9e
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-04-06 14:34:39 +0300

    added functiond to Path class

--- simple-file-manager-oop/Path.cs
+++ simple-file-manager-oop/Path.cs
@@ -2,13 +2,54 @@ namespace simple_file_manager_oop;
 
 public class Path
 {
-    public void Clear()
+    private static string _fullPath;
+    
+    /// <summary>
+    /// Очистить существующий путь
+    /// </summary>
+    public static void Clear()
     {
-        
+        _fullPath = String.Empty; 
     }
 
-    public void Set(string path)
+    /// <summary>
+    /// Изменить путь на значение
+    /// </summary>
+    /// <param name="path">Новый путь</param>
+    public static void Set(string path)
     {
-        
+        _fullPath = path;
+    }
+
+    /// <summary>
+    /// Переместиться в папку
+    /// </summary>
+    /// <param name="directory">Название папки вида "directory"</param>
+    public static void MoveTo(string directory)
+    {
+        _fullPath += "/" + directory;
+    }
+
+    /// <summary>
+    /// Покинуть директорию
+    /// Подняться на папку выше относительно текущей директории
+    /// /home/user/path/dir -> /home/user/path
+    /// </summary>
+    public static void Leave()
+    {
+        int newPathLength = _fullPath.Split('/').Length - 1;
+        string[] folders = new string[newPathLength];
+
+        for (int i = 0; i < newPathLength; i++)
+        {
+            folders[i] = _fullPath.Split('/')[i];
+        }
+
+        Clear();
+
+        for (int i = 1; i < newPathLength; i++)
+        {
+            _fullPath += "/" + folders[i];
+        }
     }
 }
