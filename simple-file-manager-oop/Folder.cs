30f2cb2ad225d78a1891db5027c5ec728ff230c6
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-04-06 12:46:45 +0300

    created Folder class

---
+++ simple-file-manager-oop/Folder.cs
@@ -0,0 +1,34 @@
+namespace simple_file_manager_oop;
+
+public class Folder : IEntry
+{
+    public void Create(string name)
+    {
+        
+    }
+
+    public void Delete()
+    {
+        
+    }
+
+    public void Rename(string name)
+    {
+        
+    }
+
+    public void Copy(string path)
+    {
+        
+    }
+
+    public void Search()
+    {
+        
+    }
+
+    public int GetSize()
+    {
+        return int.MinValue;
+    }
+}
